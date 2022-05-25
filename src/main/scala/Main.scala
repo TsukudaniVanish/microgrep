1ba9ebaab17f7625722a864f081311dd0ee762df
yuya <tsukudani710@gmail.com>
2022-05-25 12:58:55 +0900

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,12 @@
 @main def grep(keyword: String, dst: String): Unit = 
-  for s <- dst <<: keyword
-  do println(s)
+  (dst <<: keyword) foreach println
 
 extension (s:String) 
   def <<:(key:String):Iterator[String] = 
-      for line <- s.linesIterator
+    for line <- s.linesIterator
         if line.contains(key)
       yield 
         line
 
+      
 
